192398dd02a8bd8ae6693767c9f5f41796261fbc
Nilesh-chaudhary <nileshchaudhary532@gmail.com>
2022-12-19 21:46:32 +0530

    changed deprecated style

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -42,7 +42,7 @@ const Post = ({ post, setCurrentId }) => {
           size="small"
           onClick={() => setCurrentId(post._id)}
         >
-          <MoreHorizIcon fontSize="default" />
+          <MoreHorizIcon fontSize="medium" />
         </Button>
       </div>
       <div className={classes.details}>
